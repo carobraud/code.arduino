e29f507d22e87b8ceea8ce8a6c6dae045a7d33c9
Caroline Braud <caroline.braud@univ-lille1.fr>
2013-04-22 15:25:03 +0200

    Simple Blinck program ATmega2660: blinck.c.v10.0.0

--- burst.c
+++ burst.c
@@ -6,29 +6,24 @@
 
 #define LED_DDR  DDRB
 #define LED_PORT PORTB
-#define TTL_DDR  DDRD
-#define TTL_PORT PORTD
-#define LED      PB5
-#define TTL      PD7
+#define LED      PB7
 
 int main(void)
 {
   //
   int i;
   LED_DDR|=_BV(LED);//LED output
-  TTL_DDR&=~_BV(TTL);//TTL input
   //
   while(1)
   {
-    //loop_until_bit_is_set(PINB,TTL); //wait for PIV synchronization
     for(i=0;i<5;++i)
       {//burst
 	LED_PORT|=_BV(LED);//LED on
-	_delay_ms(500);
+	_delay_ms(5000);
 	LED_PORT&=~_BV(LED);//LED off
-	_delay_ms(100);
+	_delay_ms(5000);
       }
-    //    _delay_ms(1000);
+        _delay_ms(1000);
   }
   return (0);
 }
